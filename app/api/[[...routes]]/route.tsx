c5c6069961ed33192ee884f031d21af0ff4d808d
KingAceBlack <119745116+KingAceBlack@users.noreply.github.com>
2024-05-17 19:29:52 +0100

    Update route.tsx

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -101,11 +101,81 @@ app.frame('/firstchoice', (c) => {
 
 
 
-
-
-
-
 app.frame('/showPlayerStatus', (c) => {
+    let image: string;
+    let intents: JSX.Element[] = [];
+
+    // Check the different combinations of inventory slots
+    if (progressMarker.inventorySlot1 === 1 && progressMarker.inventorySlot2 === 1 && progressMarker.inventorySlot3 === 1) {
+        image = 'https://gateway.pinata.cloud/ipfs/QmfNerwFzgrmxy6VijLGamzpdKubNaK42npPUaNxHSZurW';
+    } else if (progressMarker.inventorySlot1 === 1 && progressMarker.inventorySlot2 === 0 && progressMarker.inventorySlot3 === 1) {
+        image = 'https://gateway.pinata.cloud/ipfs/QmfSd5kndQoHDqGnzSocHAen1jbWy2d6W1M9PoFCFpVWsM';
+    } else if (progressMarker.inventorySlot1 === 0 && progressMarker.inventorySlot2 === 1 && progressMarker.inventorySlot3 === 1) {
+        image = 'https://gateway.pinata.cloud/ipfs/QmZn57vyWprfyEK9nbkbgAWUmsYyFT3JeTMsGG2RoRfP6Q';
+    } else if (progressMarker.inventorySlot1 === 1 && progressMarker.inventorySlot2 === 1 && progressMarker.inventorySlot3 === 0) {
+        image = 'https://gateway.pinata.cloud/ipfs/QmViozrZ1YHyNfMpPGCJQ5dus2wXrA8oxwJiFyYMGsdvTn';
+    } else if (progressMarker.inventorySlot1 === 1 && progressMarker.inventorySlot2 === 0 && progressMarker.inventorySlot3 === 0) {
+        image = 'https://gateway.pinata.cloud/ipfs/QmXMyuj3Nc6RYEbaHHwDtmFNmFCw4Rid6tunBGxjzyp9mx';
+    } else if (progressMarker.inventorySlot1 === 0 && progressMarker.inventorySlot2 === 1 && progressMarker.inventorySlot3 === 0) {
+        image = 'https://gateway.pinata.cloud/ipfs/QmWWiJuyKX2q7QopT3Ug8NtgRQdkvKZT3MvsrFPkmtuJM4';
+    } else if (progressMarker.inventorySlot1 === 0 && progressMarker.inventorySlot2 === 0 && progressMarker.inventorySlot3 === 1) {
+        image = 'https://gateway.pinata.cloud/ipfs/QmQEqpEiKjakiro1xudf6FwDxoWsf1z7SmiMeU25G36AuX';
+    } else {
+        // Default case where all inventory slots are 0 or any unexpected combination
+        image = 'https://gateway.pinata.cloud/ipfs/QmatyUPpccdoYX9eELzF1ApFPNpkHoH4Dp8NAdCT7rfdjQ';
+    }
+
+    // Add buttons based on inventory slots
+    if (progressMarker.inventorySlot1 === 1) {
+        intents.push(<Button action="/mysticpotionused">Mystic Potion</Button>);
+    }
+    if (progressMarker.inventorySlot2 === 1) {
+        intents.push(<Button action="/medickitused">Medic Kit</Button>);
+    }
+    if (progressMarker.inventorySlot3 === 1) {
+        intents.push(<Button action="/tinkererbombused">Tinkerers Bomb</Button>);
+    }
+    if (progressMarker.backButton === 1) {
+        intents.push(<Button action={progressMarker.previousFrame}>Close</Button>);
+    }
+
+    return c.res({
+        image: (
+            <div
+                style={{
+                    position: 'relative',  // Set the container to relative positioning
+                    height: '100vh',
+                    background: 'white',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
+                <img
+                    src={image}
+                    alt="Status Image"
+                    style={{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        width: '100%',
+                        height: '100%',
+                        objectFit: 'cover',  // Ensure the image covers the entire container
+                    }}
+                />
+                <p style={{ fontSize: '38px', margin: '0', marginTop: '-514px', color: 'green', textAlign: 'right', marginRight: '-892px', fontWeight: 'bold' }}>
+                    {player.life}
+                </p>
+            </div>
+        ),
+        intents: intents
+    });
+});
+
+
+
+
+/*app.frame('/depreciated--showPlayerStatus', (c) => {
     let image;
     let intents = [];
 
@@ -325,7 +395,7 @@ app.frame('/showPlayerStatus', (c) => {
         image: image,
         intents: intents
     });
-});
+});*/
 
 
 
